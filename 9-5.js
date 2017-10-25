633ed7931f6c885d5531b577ccdd5f9533304fe7
Monika Stokowska <30418684+Monika-Stokowska@users.noreply.github.com>
2017-10-25 21:48:03 +0200

    zadanie 9-5-nie skonczone

---
+++ 9-5.js
@@ -0,0 +1,8 @@
+var btn = document.createElement("BUTTON");        
+var t = document.createTextNode("CLICK ME");       
+btn.appendChild(t);                                
+document.body.appendChild(btn);    
+
+var butonki = document.getElementsByClassName('button');
+document.getElementById("BUTTON").className = "button";
+console.log(butonki);
